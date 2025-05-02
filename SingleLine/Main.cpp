62e2bd016cae892b7bda6fed62932d21f1049311
ryota37 <ryota.nakahama@access-company.com>
2025-05-02 21:26:10 +0900

    Blocked re-entry to visited grid

--- SingleLine/Main.cpp
+++ SingleLine/Main.cpp
@@ -41,36 +41,60 @@ void DrawCircle(const Circle& circle)
 	circle.draw(Palette::Aqua);
 }
 
-bool isWallUp(Circle& circle)
+bool isWallUp(Circle& circle, Grid<int32>& grid)
 {
+	int grid_x = static_cast<int>(circle.x) / 100;
+	int grid_y = static_cast<int>(circle.y) / 100;
+	int grid_y_up = std::max(grid_y - 1, 0);
+
 	if (circle.y == 50) return true;
+	if (grid[grid_y_up][grid_x] > 0) return true;
+
 	return false;
 }
 
-bool isWallLeft(Circle& circle)
+bool isWallLeft(Circle& circle, Grid<int32>& grid)
 {
+	int grid_x = static_cast<int>(circle.x) / 100;
+	int grid_y = static_cast<int>(circle.y) / 100;
+	int grid_x_left = std::max(grid_x - 1, 0);
+
 	if (circle.x == 50) return true;
+	if (grid[grid_y][grid_x_left] > 0) return true;
+
 	return false;
 }
 
-bool isWallDown(Circle& circle)
+bool isWallDown(Circle& circle, Grid<int32>& grid)
 {
+	int grid_x = static_cast<int>(circle.x) / 100;
+	int grid_y = static_cast<int>(circle.y) / 100;
+	int grid_y_down = std::min(grid_y + 1, 7);
+
 	if (circle.y == 550) return true;
+	if (grid[grid_y_down][grid_x] > 0) return true;
+
 	return false;
 }
 
-bool isWallRight(Circle& circle)
+bool isWallRight(Circle& circle, Grid<int32>& grid)
 {
+	int grid_x = static_cast<int>(circle.x) / 100;
+	int grid_y = static_cast<int>(circle.y) / 100;
+	int grid_x_right = std::min(grid_x + 1, 5);
+
 	if (circle.x == 750) return true;
+	if (grid[grid_y][grid_x_right] > 0) return true;
+
 	return false;
 }
 
-void UpdateCircle(Circle& circle)
+void UpdateCircle(Circle& circle, Grid<int32>& grid)
 {
-	if (KeyLeft.down() && !isWallLeft(circle)) circle.x -= 100;
-	if (KeyUp.down() && !isWallUp(circle)) circle.y -= 100;
-	if (KeyRight.down() && !isWallRight(circle)) circle.x += 100;
-	if (KeyDown.down() && !isWallDown(circle)) circle.y += 100;
+	if (KeyLeft.down() && !isWallLeft(circle, grid)) circle.x -= 100;
+	if (KeyUp.down() && !isWallUp(circle, grid)) circle.y -= 100;
+	if (KeyRight.down() && !isWallRight(circle, grid)) circle.x += 100;
+	if (KeyDown.down() && !isWallDown(circle, grid)) circle.y += 100;
 }
 
 void PrintGoal(const Circle& circle)
@@ -124,7 +148,7 @@ void Main()
 
 		DrawGrid(grid);
 
-		UpdateCircle(circle);
+		UpdateCircle(circle, grid);
 
 		DrawCircle(circle);
 
